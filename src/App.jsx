88d3045cb1b50ffdc6996dc1d7ead27c18701d17
erynder-z <stfn.bgr@gmail.com>
2022-05-21 20:53:58 +0200

    Fix app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
 import './App.css';
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut,
-  connectAuthEmulator,
-  AuthErrorCodes,
-  GoogleAuthProvider,
-  signInWithPopup
-} from 'firebase/auth';
-import { getFirestore, setDoc, doc, getDoc, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import SignIn from './components/SignIn/SignIn';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import './components/SignIn/SignIn.css';
 import Chatroom from './components/Chatroom/Chatroom';
+import SignIn from './components/SignIn/SignIn';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB5TIKYS0o_XURgZf8ot_BgQ8SPtBNTE9A',
+
+  authDomain: 'chatpack-d9c39.firebaseapp.com',
+
+  projectId: 'chatpack-d9c39',
+
+  storageBucket: 'chatpack-d9c39.appspot.com',
+
+  messagingSenderId: '36648908277',
+
+  appId: '1:36648908277:web:3a9b1882d7616236271cbc'
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+connectAuthEmulator(auth, 'http://localhost:9099');
+connectFirestoreEmulator(firestore, 'localhost', 8080);
 
 function App() {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyB5TIKYS0o_XURgZf8ot_BgQ8SPtBNTE9A',
-    authDomain: 'chatpack-d9c39.firebaseapp.com',
-    projectId: 'chatpack-d9c39',
-    storageBucket: 'chatpack-d9c39.appspot.com',
-    messagingSenderId: '36648908277',
-    appId: '1:36648908277:web:3a9b1882d7616236271cbc'
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
   const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header" />
-      <section>{user ? <Chatroom auth={auth} /> : <SignIn />}</section>
+      <section>{user ? <Chatroom auth={auth} firestore={firestore} /> : <SignIn />}</section>
     </div>
   );
 }
