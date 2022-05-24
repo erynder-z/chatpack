25c1064787ea544b826246268f620807a0b3a866
erynder-z <stfn.bgr@gmail.com>
2022-05-24 22:15:39 +0200

    Display currently online users

---
+++ src/components/OnlineUsers/OnlineUsers.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { collection, query } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import './OnlineUsers.css';
+
+function OnlineUsers({ firestore }) {
+  const [showOnline, setShowOnline] = useState(false);
+
+  const messagesRef = collection(firestore, 'onlineUsers');
+  const q = query(messagesRef);
+  const [onlineUsers] = useCollectionData(q, { idField: 'id' });
+
+  const toggle = () => {
+    setShowOnline(!showOnline);
+  };
+
+  return (
+    <div className={`online-users ${showOnline ? 'active' : ''}`}>
+      <div className="usr-container">
+        {onlineUsers &&
+          showOnline &&
+          onlineUsers.map((usr) => {
+            return (
+              <li key={onlineUsers.indexOf(usr).toString()} className="online-usr">
+                {usr.name}
+              </li>
+            );
+          })}
+      </div>
+      <button className="online-users-toggle" onClick={toggle} type="button">
+        <BsThreeDotsVertical size="1rem" />
+      </button>
+    </div>
+  );
+}
+
+export default OnlineUsers;
