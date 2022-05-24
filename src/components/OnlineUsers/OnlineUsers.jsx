c25a584abe3afb2a56b679853a32d831e316e7dd
erynder-z <stfn.bgr@gmail.com>
2022-05-24 22:52:57 +0200

    Show online users only if signed in

--- src/components/OnlineUsers/OnlineUsers.jsx
+++ src/components/OnlineUsers/OnlineUsers.jsx
@@ -4,9 +4,11 @@ import { collection, query } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import './OnlineUsers.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-function OnlineUsers({ firestore }) {
+function OnlineUsers({ auth, firestore }) {
   const [showOnline, setShowOnline] = useState(false);
+  const [user] = useAuthState(auth);
 
   const messagesRef = collection(firestore, 'onlineUsers');
   const q = query(messagesRef);
@@ -18,21 +20,25 @@ function OnlineUsers({ firestore }) {
 
   return (
     <div className={`online-users ${showOnline ? 'active' : ''}`}>
-      <div className="usr-container">
-        <h4>online users:</h4>
-        {onlineUsers &&
-          showOnline &&
-          onlineUsers.map((usr) => {
-            return (
-              <li key={onlineUsers.indexOf(usr).toString()} className="online-usr">
-                {usr.name}
-              </li>
-            );
-          })}
-      </div>
-      <button className="online-users-toggle" onClick={toggle} type="button">
-        <BsThreeDotsVertical size="1rem" color="whitesmoke" />
-      </button>
+      {user && (
+        <>
+          <div className="usr-container">
+            <h4>online users:</h4>
+            {onlineUsers &&
+              showOnline &&
+              onlineUsers.map((usr) => {
+                return (
+                  <li key={onlineUsers.indexOf(usr).toString()} className="online-usr">
+                    {usr.name}
+                  </li>
+                );
+              })}
+          </div>
+          <button className="online-users-toggle" onClick={toggle} type="button">
+            <BsThreeDotsVertical size="1rem" color="whitesmoke" />
+          </button>
+        </>
+      )}
     </div>
   );
 }
