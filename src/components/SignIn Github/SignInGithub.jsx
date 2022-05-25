573102d2b6e3abcbba952ec11a283c68c161736a
erynder-z <stfn.bgr@gmail.com>
2022-05-25 17:06:45 +0200

    Add signin with github

---
+++ src/components/SignIn Github/SignInGithub.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
+import { AiOutlineGithub } from 'react-icons/ai';
+import './SignInGithub.css';
+
+function SignInGithub() {
+  async function signInWithGithub() {
+    const provider = new GithubAuthProvider();
+
+    await signInWithPopup(getAuth(), provider);
+  }
+
+  return (
+    <button className="signinBtnGithub" onClick={signInWithGithub} type="button">
+      <div className="signin-container">
+        <div className="signin-text">Sign in with Github</div>
+        <div className="divider" />
+        <AiOutlineGithub size="4rem" />
+      </div>
+    </button>
+  );
+}
+
+export default SignInGithub;
