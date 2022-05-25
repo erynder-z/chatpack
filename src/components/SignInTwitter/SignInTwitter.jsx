c71a5f948bf9e7bef4f5c883368503de68a3603a
erynder-z <stfn.bgr@gmail.com>
2022-05-25 22:29:40 +0200

    Fix twitter login

--- src/components/SignInTwitter/SignInTwitter.jsx
+++ src/components/SignInTwitter/SignInTwitter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { getAuth, signInWithPopup, TwitterAuthProvider } from 'firebase/auth';
 import { AiOutlineTwitter } from 'react-icons/ai';
@@ -7,7 +8,24 @@ function SignInTwitter() {
   async function signInWithTwitter() {
     const provider = new TwitterAuthProvider();
 
-    await signInWithPopup(getAuth(), provider);
+    const auth = getAuth();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = TwitterAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const { secret } = credential;
+
+        const { user } = result;
+      })
+      .catch((error) => {
+        alert(`Something went wrong: ${error.message}`);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const { email } = error.customData;
+
+        const credential = TwitterAuthProvider.credentialFromError(error);
+      });
   }
 
   return (
