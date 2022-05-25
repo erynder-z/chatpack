781d22252e6f90825da43342247f6f1158b71886
erynder-z <stfn.bgr@gmail.com>
2022-05-25 21:27:59 +0200

    Fix github login

--- src/components/SignIn Github/SignInGithub.jsx
+++ src/components/SignIn Github/SignInGithub.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
 import { AiOutlineGithub } from 'react-icons/ai';
@@ -7,7 +8,23 @@ function SignInGithub() {
   async function signInWithGithub() {
     const provider = new GithubAuthProvider();
 
-    await signInWithPopup(getAuth(), provider);
+    const auth = getAuth();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        const { user } = result;
+      })
+      .catch((error) => {
+        alert(`Something went wrong: ${error.message}`);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const { email } = error.customData;
+
+        const credential = GithubAuthProvider.credentialFromError(error);
+      });
   }
 
   return (
